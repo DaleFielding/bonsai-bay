7e50f83927ceb4f1e3e5a87487535a234b7d90a7
DaleFielding <dalefielding28@gmail.com>
2024-04-09 18:50:03 +0000

    Feat: add route to display listings, fix image upload with encoding and rename tree route to listing

--- bonsai_bay/routes.py
+++ bonsai_bay/routes.py
@@ -1,11 +1,14 @@
 from flask import render_template, request, redirect, url_for
 from bonsai_bay import app, db
-from bonsai_bay.models import Tree
+from bonsai_bay.models import Listing, User, Seller, SavedItem
+import base64
+
 
 # Homepage
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 # Homepage - scrolled to browse_bonsai id
 @app.route("/")
@@ -16,13 +19,38 @@ def browse_bonsai():
 # Account Page
 @app.route("/account")
 def account():
-    return render_template("account.html")
+     # Query the database to retrieve all listings
+    all_listings = Listing.query.all()
+    
+    # Iterate over each listing to encode its image
+    for listing in all_listings:
+        if listing.image:
+            # Encode image data to Base64
+            listing.encoded_image = base64.b64encode(listing.image).decode('utf-8')
+        else:
+            listing.encoded_image = None
+    
+    # Render account page and pass listings to the template
+    return render_template("account.html", listings=all_listings)
 
 # Create Listing
 @app.route("/create_listing", methods=["GET", "POST"])
 def create_listing():
     if request.method == "POST":
-        tree = Tree (
+        # Adding to help with determining issue with database
+        app.logger.info("Form data received: %s", request.form)
+        
+        # To handle the uploaded files
+        image_file = request.files['image']
+        if image_file:
+            image_data = image_file.read()
+            encoded_image = base64.b64encode(image_data).decode('utf-8')
+        else:
+            image_data = None
+            encoded_image = None
+
+        # Create new listing object using my form data
+        listing = Listing(
             title=request.form.get("title"),
             species=request.form.get("species"),
             tree_type=request.form.get("tree_type"),
@@ -31,11 +59,14 @@ def create_listing():
             height=request.form.get("height"),
             price=request.form.get("price"),
             care_tips=request.form.get("care_tips"),
-            image=request.form.get("image"),
+            image=image_data
         )
-        db.session.add(tree)
+        db.session.add(listing)
         db.session.commit()
-        return redirect(url_for("accounts"))
+        
+        return redirect(url_for("account"))
+
+
     return render_template("account.html")
 
 
