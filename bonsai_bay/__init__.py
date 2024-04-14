a229f9691cb3669f956b46a9ef9e6008b2c99d3d
DaleFielding <dalefielding28@gmail.com>
2024-04-14 18:57:05 +0000

    Feat: import LoginManager and add function to load a user from their Id

--- bonsai_bay/__init__.py
+++ bonsai_bay/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 # env is only imported if the os can find an existing filepath for the env.py file
 if os.path.exists("env.py"):
@@ -12,5 +14,12 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+# The below loads a user from the database from their ID.
+@login_manager.user_loader
+def load_user(user_id):
+    from bonsai_bay.models import User
+    return User.query.get(int(user_id))
 
 from bonsai_bay import routes 
