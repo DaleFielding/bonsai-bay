2d88c2a13cc6eece670105469bdc3dd1dd6c6fad
DaleFielding <dalefielding28@gmail.com>
2024-04-15 12:53:07 +0000

    Feat: add functionality to display search form input into the console.

--- bonsai_bay/static/js/search.js
+++ bonsai_bay/static/js/search.js
@@ -12,3 +12,26 @@ document.addEventListener('DOMContentLoaded', function () {
     searchBar.classList.toggle('d-none');
   });
 });
+
+/*
+Functionality to:
+* Access attributes for search form, input and button 
+* When pressing enter or click on the search icon button; 
+logs input to the console.
+*/
+document.addEventListener("DOMContentLoaded", function () {
+  const searchForm = document.getElementById("searchForm");
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let searchInput = document.querySelector(".form-control").value;
+    console.log("Search query:", searchInput);
+  });
+
+  let button = document.querySelector(".search-btn");
+
+  button.addEventListener("click", function (event) {
+
+    event.preventDefault();
+    searchForm.dispatchEvent(new Event("submit"));
+  });
+});
