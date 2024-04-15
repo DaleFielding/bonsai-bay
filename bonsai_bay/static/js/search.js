2ab25a8d468a7d48e74e2b4c3a7167d92626021b
DaleFielding <dalefielding28@gmail.com>
2024-04-15 15:59:07 +0000

    Feat: add functionality to search listings and display result

--- bonsai_bay/static/js/search.js
+++ bonsai_bay/static/js/search.js
@@ -13,25 +13,44 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-/*
-Functionality to:
-* Access attributes for search form, input and button 
-* When pressing enter or click on the search icon button; 
-logs input to the console.
-*/
-document.addEventListener("DOMContentLoaded", function () {
-  const searchForm = document.getElementById("searchForm");
-  searchForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    let searchInput = document.querySelector(".form-control").value;
-    console.log("Search query:", searchInput);
-  });
 
-  let button = document.querySelector(".search-btn");
+/**  searchListings() function: 
+* Accesses the search query id from base.html
+* Send the search query input as a request parmeter using fetch.
+* Gets a json response with the data.
+* Clears the listings currently displayed in homepage.
+* Iterates through the response data
+* Appends newly created item cards with the listings in the response data.
+* Log errors to console if issues.
+**/
 
-  button.addEventListener("click", function (event) {
+function searchListings() {
+  let query = document.getElementById("search-query").value.trim();
+  fetch(`/search?query=${query}`)
+    .then(response => response.json())
+    .then(data => {
 
-    event.preventDefault();
-    searchForm.dispatchEvent(new Event("submit"));
-  });
-});
+      let listingsContainer = document.querySelector(".listings");
+      listingsContainer.innerHTML = "";
+
+      data.forEach(listing => {
+        let listingDiv = document.createElement("div");
+        listingDiv.classList.add("col-10", "col-sm-6", "col-md-4", "col-lg-3");
+        listingDiv.innerHTML = `
+          <div class="item-card-container">
+            <a href="/item/${listing.id}" class="item-card-link">
+              <div class="item-card item-card-small">
+                <div class="listing">
+                  <h6>${listing.title}</h6>
+                </div>
+                ${listing.image ? `<img src="data:image/jpeg;base64,${listing.image}" alt="${listing.title} Image" style="max-width: 100%; height: auto" class="img-fluid">` : '<p>No image available</p>'}
+                <p>&pound;${listing.price}</p>
+              </div>
+            </a>
+          </div>
+        `;
+        listingsContainer.appendChild(listingDiv);
+      });
+    })
+    .catch(error => console.error('Error:', error));
+}
