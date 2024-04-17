6fdf8d9af66fdd71b50d0f41d0dda5a22587338c
DaleFielding <dalefielding28@gmail.com>
2024-04-17 19:17:28 +0000

    Feat: add functionality to handle no results found when searching

--- bonsai_bay/static/js/search.js
+++ bonsai_bay/static/js/search.js
@@ -21,12 +21,14 @@ document.getElementById("search-query").addEventListener("keydown", function (ev
   }
 });
 
-/**  searchListings() function: 
+/**  searchListings() function:
 * Accesses the search query id from base.html
 * Send the search query input as a request parmeter using fetch.
 * Gets a json response with the data.
 * Clears the listings currently displayed in homepage.
-* Iterates through the response data
+* Checks if there are no matching results
+* If so it displays "No results found for (query)"
+* If there are results it iterates through the response data...
 * Appends newly created item cards with the listings in the response data.
 * After this, scrolls down to where the listings are displayed.
 * Log errors to console if issues.
@@ -40,24 +42,29 @@ function searchListings() {
       let listingsContainer = document.querySelector(".listings");
       listingsContainer.innerHTML = "";
 
-      data.forEach(listing => {
-        let listingDiv = document.createElement("div");
-        listingDiv.classList.add("col-10", "col-sm-6", "col-md-4", "col-lg-3");
-        listingDiv.innerHTML = `
-          <div class="item-card-container">
-            <a href="/item/${listing.id}" class="item-card-link">
-              <div class="item-card item-card-small">
-                <div class="listing">
-                  <h6>${listing.title}</h6>
+      if (data.length === 0) {
+        // Display a message when there are no matching results
+        listingsContainer.innerHTML = `<p class="m-3">No results found for ${query}</p>`;
+      } else {
+        data.forEach(listing => {
+          let listingDiv = document.createElement("div");
+          listingDiv.classList.add("col-10", "col-sm-6", "col-md-4", "col-lg-3");
+          listingDiv.innerHTML = `
+            <div class="item-card-container">
+              <a href="/item/${listing.id}" class="item-card-link">
+                <div class="item-card item-card-small">
+                  <div class="listing">
+                    <h6>${listing.title}</h6>
+                  </div>
+                  ${listing.image ? `<img src="data:image/jpeg;base64,${listing.image}" alt="${listing.title} Image" style="max-width: 100%; height: auto" class="img-fluid">` : '<p>No image available</p>'}
+                  <p>£${listing.price}</p>
                 </div>
-                ${listing.image ? `<img src="data:image/jpeg;base64,${listing.image}" alt="${listing.title} Image" style="max-width: 100%; height: auto" class="img-fluid">` : '<p>No image available</p>'}
-                <p>&pound;${listing.price}</p>
-              </div>
-            </a>
-          </div>
-        `;
-        listingsContainer.appendChild(listingDiv);
-      });
+              </a>
+            </div>
+          `;
+          listingsContainer.appendChild(listingDiv);
+        });
+      }
       const scrollToListings = document.getElementById("browse-bonsai");
       scrollToListings.scrollIntoView({ behavior: 'smooth' });
     })
