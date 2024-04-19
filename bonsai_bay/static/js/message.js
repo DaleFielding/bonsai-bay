55628f7658f4f9c49caa663e354442278ed7b54c
DaleFielding <dalefielding28@gmail.com>
2024-04-19 14:33:03 +0000

    Feat: add messageSent function to change html when button clicked

---
+++ bonsai_bay/static/js/message.js
@@ -0,0 +1,14 @@
+
+/** messageSent function:
+* Prevent defaults submission.
+* Accesses the data attribute data-message-sent.
+* Changes the innerHtml when send button is clicked
+* Message times out after 2 seconds, changing innerHTML
+**/
+function messageSent(event) {
+  event.preventDefault();
+  document.querySelector('[data-message-sent]').innerHTML = 'Your message has been sent';
+  setTimeout(function () {
+    document.querySelector('[data-message-sent]').innerHTML = '&nbsp;';
+  }, 2000);
+}
