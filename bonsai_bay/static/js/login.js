cfec4bda6e6ad26e801b18337b976f602cdf6e7c
DaleFielding <dalefielding28@gmail.com>
2024-04-20 06:56:44 +0000

    Fix: make appropriate changes to ensure login.js passes JSlinting

--- bonsai_bay/static/js/login.js
+++ bonsai_bay/static/js/login.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   * Access the loginForm element and prevent default submission
   * An event listener that is triggered when the form's login button is clicked.
   * Use built in FormData constructor to build a set of key value pairs.
-  * Then POST form data to server, then parse response as json 
+  * Then POST form data to server, then parse response as json.
   * Access the login-message data attribute for message to be displayed.
   * Then display either success or error and include bootstrap alert classes.
   * If login is successful, redirect to account page.
@@ -18,22 +18,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const formData = new FormData(loginForm);
 
     fetch(loginForm.action, {
-      method: "POST",
-      body: formData
+      body: formData,
+      method: "POST"
     })
-      .then(response => response.json())
-      .then(data => {
-
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
         const loginMessage = document.querySelector("[data-login-message]");
 
         if (data.success) {
-          loginMessage.innerHTML = `<div class="alert alert-success">${data.message}</div>`;
+          loginMessage.innerHTML =
+            `<div class="alert alert-success">${data.message}</div>`;
           window.location.href = "/account";
         } else {
-          loginMessage.innerHTML = `<div class="alert alert-danger">${data.message}</div>`;
+          loginMessage.innerHTML =
+            `<div class="alert alert-danger">${data.message}</div>`;
         }
       })
-      .catch(error => {
+      .catch(function (error) {
         console.error("An error has occurred =", error);
       });
   });
@@ -46,13 +49,13 @@ document.addEventListener("DOMContentLoaded", function () {
 * If not logged in the login/register modal pops up.
 */
 $(document).ready(function () {
-  $('[data-account-link]').click(function (e) {
+  $("[data-account-link]").click(function (e) {
     e.preventDefault();
-    $.get('/is_logged_in', function (data) {
+    $.get("/is_logged_in", function (data) {
       if (data.logged_in) {
-        window.location.href = '/account';
+        window.location.href = "/account";
       } else {
-        $('#loginRegisterModal').modal('show');
+        $("#loginRegisterModal").modal("show");
       }
     });
   });
