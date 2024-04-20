6611a8f3188d2b1ac9b0a15076975d13c4174871
DaleFielding <dalefielding28@gmail.com>
2024-04-20 07:49:15 +0000

    Fix: make appropriate changes to ensure search.js passes JSlint

--- bonsai_bay/static/js/search.js
+++ bonsai_bay/static/js/search.js
@@ -1,33 +1,35 @@
-/* 
+/*
 * Accesses the data attributes for search link and search bar
 * Adds a click event on the search, including prevent default.
 * Use to toggle to add or remove d-none from the classlist
 */
-document.addEventListener('DOMContentLoaded', function () {
-  const searchLink = document.querySelector('[data-toggle-search]');
-  const searchBar = document.querySelector('[data-search-bar]');
+document.addEventListener("DOMContentLoaded", function () {
+  const searchLink = document.querySelector("[data-toggle-search]");
+  const searchBar = document.querySelector("[data-search-bar]");
 
-  searchLink.addEventListener('click', function (event) {
+  searchLink.addEventListener("click", function (event) {
     event.preventDefault();
-    searchBar.classList.toggle('d-none');
+    searchBar.classList.toggle("d-none");
   });
 });
 
-// Event listener to call the searchListings (below) function when pressing enter on the keyboard
-document.getElementById("search-query").addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    searchListings();
-  }
-});
+/* Event listener to call the searchListings function
+when pressing enter on the keyboard */
+document.getElementById("search-query")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      searchListings();
+    }
+  });
 
-/**  searchListings() function:
-* Accesses the search query id from base.html
+/** searchListings() function:
+* Accesses the search query id from base.html.
 * Send the search query input as a request parmeter using fetch.
 * Gets a json response with the data.
 * Clears the listings currently displayed in homepage.
-* Checks if there are no matching results
-* If so it displays "No results found for (query)"
+* Checks if there are no matching results.
+* If so it displays "No results found for (query)".
 * If there are results it iterates through the response data...
 * Appends newly created item cards with the listings in the response data.
 * After this, scrolls down to where the listings are displayed.
@@ -44,11 +46,13 @@ function searchListings() {
 
       if (data.length === 0) {
         // Display a message when there are no matching results
-        listingsContainer.innerHTML = `<p class="m-3">No results found for ${query}</p>`;
+        listingsContainer.innerHTML =
+          `<p class="m-3">No results found for ${query}</p>`;
       } else {
         data.forEach(listing => {
           let listingDiv = document.createElement("div");
-          listingDiv.classList.add("col-10", "col-sm-6", "col-md-4", "col-lg-3");
+          listingDiv.classList
+            .add("col-10", "col-sm-6", "col-md-4", "col-lg-3");
           listingDiv.innerHTML = `
             <div class="item-card-container">
               <a href="/item/${listing.id}" class="item-card-link">
@@ -56,7 +60,11 @@ function searchListings() {
                   <div class="listing">
                     <h6>${listing.title}</h6>
                   </div>
-                  ${listing.image ? `<img src="data:image/jpeg;base64,${listing.image}" alt="${listing.title} Image" style="max-width: 100%; height: auto" class="img-fluid">` : '<p>No image available</p>'}
+                  ${listing.image ?
+              `<img src="data:image/jpeg;base64,${listing.image}" alt="${listing.title}
+                    Image" style="max-width: 100%; height: auto" class="img-fluid">`
+              :
+              "<p>No image available</p>"}
                   <p>£${listing.price}</p>
                 </div>
               </a>
@@ -66,7 +74,7 @@ function searchListings() {
         });
       }
       const scrollToListings = document.getElementById("browse-bonsai");
-      scrollToListings.scrollIntoView({ behavior: 'smooth' });
+      scrollToListings.scrollIntoView({ behavior: "smooth" });
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => console.error("Error:", error));
 }
